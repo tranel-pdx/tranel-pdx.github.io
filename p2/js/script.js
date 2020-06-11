c0340395e8220c89513bcc200c8be2f20f35fc89
tranel-pdx <59620262+tranel-pdx@users.noreply.github.com>
2020-06-11 11:11:30 -0700

    dear god it clicked

--- p2/js/script.js
+++ p2/js/script.js
@@ -1,7 +1,21 @@
 // your script file for adding your own jquery
 $(function() {
 // Your Code from here on down. Don't delete that line above!
-
+$('.btn').click(function(){
+      $(this).toggleClass("click");
+      $('.sidebar').toggleClass("show");
+    });
+      $('.feat-btn').click(function(){
+        $('nav ul .feat-show').toggleClass("show");
+        $('nav ul .first').toggleClass("rotate");
+      });
+      $('.serv-btn').click(function(){
+        $('nav ul .serv-show').toggleClass("show1");
+        $('nav ul .second').toggleClass("rotate");
+      });
+      $('nav ul li').click(function(){
+        $(this).addClass("active").siblings().removeClass("active");
+      });
 
 
 
